8dc45615a5d3e7495bc4a1b412a3be011bea1e5b
ryo_adachi <t.virus.1989@gmail.com>
2018-03-26 10:32:56 +0900

    variables-with-initiallzers.go

---
+++ go-tour/packages-variables-functions/variables-with-initiallzers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var adachinyo, adachin, adachinpo = true, false, "no!"
+	fmt.Println(i, j, adachinyo, adachin, adachinpo)
+}
+
---
+++ go-tour/packages-variables-functions/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var adachinyo, adachin, adachinpo bool
+
+func main() {
+	var i int
+	fmt.Println(i, adachinyo, adachin, adachinpo)
+}
+
