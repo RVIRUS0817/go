0d6aee5f95f0b979c420a4d2538d7d4781853be8
ryo_adachi <t.virus.1989@gmail.com>
2018-03-27 14:22:12 +0900

    fix go

--- adachi3/adachi3.go
+++ adachi3/adachi3.go
@@ -5,28 +5,28 @@ import (
     "strconv"
 )
 
-func main() {
+//func main() {
+//
+//    fmt.Print("いくつまでやってほしいのだね？: ")
+//    var input int
+//    fmt.Scanf("%d", &input)
+//
+//    for i := 1; i <= input; i++ {
+//        fizzbuzz(i)
+//    }
+//}
+//
+//func isThreeIncluded(num int) bool {
+//    var strInt = strconv.Itoa(num)
+//    for k := 0; k < len([]rune(strInt)); k++ {
+//        if '3' == strInt[k] {
+//            return true
+//        }
+//    }
+//    return false
+//}
 
-    fmt.Print("いくつまでやってほしいのだね？: ")
-    var input int
-    fmt.Scanf("%d", &input)
-
-    for i := 1; i <= input; i++ {
-        fizzbuzz(i)
-    }
-}
-
-func isThreeIncluded(num int) bool {
-    var strInt = strconv.Itoa(num)
-    for k := 0; k < len([]rune(strInt)); k++ {
-        if '3' == strInt[k] {
-            return true
-        }
-    }
-    return false
-}
-
-func fizzbuzz(i int) {
+func main(i int) {
     adachin := "あだちん！"
 
     if isThreeIncluded(i) && i%3 == 0 {
